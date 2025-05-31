1c64e1b7fcd7ba6c9d5bba29cd6933f998a95000
dw2000 <wanger.david@gmail.com>
2025-05-31 13:57:59 +0200

    adding launcher stuff

---
+++ Houdini/pipeline/launchers/__Create_Windows_Houdini_shortcut.js
@@ -0,0 +1,18 @@
+var WshNetwork = new ActiveXObject("WScript.Network");
+var userName = WshNetwork.UserName;
+
+Shell = new ActiveXObject("WScript.Shell");
+var currentDirectory = Shell.CurrentDirectory;
+
+// Split the current directory path by the backslash delimiter
+var pathParts = currentDirectory.split("\\");
+var grandparentDirectoryName = pathParts[pathParts.length - 3];
+
+link = Shell.CreateShortcut(currentDirectory + "\\" + grandparentDirectoryName + "_Houdini_" + userName + ".lnk");
+link.WorkingDirectory = currentDirectory;
+link.Description = "Starts Houdini in a pipeline environment";
+link.IconLocation = currentDirectory + "\\Houdini.ico,0";
+link.TargetPath = currentDirectory + "\\start_Windows_Houdini_in_this_job.bat";
+link.WindowStyle = 7;
+link.Save();
+
---
+++ Houdini/pipeline/launchers/start_Windows_Houdini_in_this_job.bat
@@ -0,0 +1,8 @@
+
+@ECHO Pointing to this job's Package directory:
+set HOUDINI_PACKAGE_DIR=%cd%\..\houdini_project_library\packages;%HOUDINI_PACKAGE_DIR%
+@ECHO(
+@ECHO(
+@ECHO Starting Houdini:
+start "" /b "C:\Program Files\Side Effects Software\Houdini 20.5.584\bin\houdini.exe"
+
